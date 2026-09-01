1c3c1d274b19ba82532af5deb6fdce4d11b2e2e0
Uberkill <37767247+Uberkill@users.noreply.github.com>
2026-09-01 23:24:08 +0800

    Update vite config to use dynamic repo base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/Horizon/' : '/',
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   plugins: [
     react(),
     VitePWA({
